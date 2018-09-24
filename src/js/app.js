1c60134be4fceba50c267da09cc6e19ff2d4992d
Pablo Colaiacovo <pablo.colaiacovo@gmail.com>
2018-09-23 23:39:36 -0300

    Added countdown

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Check if there are any navbar burgers
     if ($navbarBurgers.length > 0) {
-
         // Add a click event on each of them
         $navbarBurgers.forEach(el => {
             el.addEventListener('click', () => {
@@ -16,8 +15,14 @@ document.addEventListener('DOMContentLoaded', () => {
                 // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
                 el.classList.toggle('is-active');
                 $target.classList.toggle('is-active');
-
             });
         });
     }
+
+    // var deadline = new Date(2018, 09, 26, 17, 0, 0);
+    // initializeClock('clockdiv', deadline);
+
+    $('#clock').countdown('2018/10/26 17:00', function (event) {
+        $(this).html(event.strftime('%D Días %H:%M:%S'));
+    });
 });
--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,4 +1,7 @@
 import '../scss/main.scss'
 
+import jQuery from 'jquery';
+window.$ = window.jQuery = jQuery;
 import _ from 'lodash';
 import '@fortawesome/fontawesome-free/js/all.js'
+import 'jquery-countdown';
