0c119bee4d683d70e3077f1a9c11e9ef26000d8c
Pablo Colaiacovo <pablo.colaiacovo@gmail.com>
2019-05-04 15:30:09 -0300

    Package updates

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -2,6 +2,5 @@ import '../scss/main.scss'
 
 import jQuery from 'jquery';
 window.$ = window.jQuery = jQuery;
-import _ from 'lodash';
 import '@fortawesome/fontawesome-free/js/all.js'
 import 'jquery-countdown';
